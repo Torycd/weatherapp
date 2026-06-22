a42a1091bbe8231e0c8ef56a311b8772e17bf2e5
Victory Idundun <vicidundun@gmail.com>
2026-06-22 19:37:53 +0100

    Edit layout

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Main from "./components/Main";
 
 const App = () => {
   return (
-    <div className="sm:px-[240px] py-4 bg-blue-950 text-[18px] text-white max-h-screen">
+    <div className="sm:px-[240px] py-4 bg-blue-950 text-[18px] text-white h-screen">
       <NavBar />
       <Main />
     </div>
